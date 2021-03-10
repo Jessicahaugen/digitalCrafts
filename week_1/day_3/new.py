1ed1f0c03d1e9e0c459606389f091777ece5301c
Jessica Haugen <j.rose.haugen@gmail.com>
2021-03-10 08:24:41 -0800

    first program

---
+++ week_1/day_3/new.py
@@ -0,0 +1,18 @@
+#Variable
+#  nameOfVar =  assignmenedValueToVariable 
+# integer
+classSize = 23 
+# string
+greeting = "Hello World"
+# float = 2.323
+# Boolean === True or False, Truthy or Falsy 
+sumOfNumbers = 1+1  
+varName = True 
+# List or Array = sequence of valuesc inside [ ] 
+# Nested List = [[list ], inside of a list"]
+# DIctionary (in python) Object (in Javascript)  = sequence of labled values {key: values}
+# ex. {favorite color : red}
+# paramaters inside funcitons () 
+
+print(greeting)
+
