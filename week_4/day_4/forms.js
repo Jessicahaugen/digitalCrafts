0003e3a30e1eac1e45e83b2a1a170693c166c0b7
Jessicahaugen <j.rose.haugen@gmail.com>
2021-04-01 12:59:24 -0700

    classwork

---
+++ week_4/day_4/forms.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form")
+
+const validateName = (name) => {
+    console.log(`your name is ${name}`)
+    myRegex = /[A-Za-Z] +/
+    retunr myRegex.test(name);
+
+}
+form.addEventListener("submit", (event) => {
+    const inputField = document.querySelector("#name");
+    event.preventDefault()  
+    if (!validateName(name.value)){
+    event.preventDefault()
+   }
+})
+
+form.addEventListener("submit", (event) => {
+    const textInput = document.querySelector("#bill");
+    if (!validateName(textInput.value)) {
+      event.preventDefault();
+      alert("Please enter in a valid name that doesn't contain letters");
+    }
+  });
