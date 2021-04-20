7e8a1a71bbe38771e583882a5c0852d72bac386e
Jessicahaugen <j.rose.haugen@gmail.com>
2021-04-20 13:39:46 -0700

    cleaned up

--- week_7/day_2/index.js
+++ week_7/day_2/index.js
@@ -3,10 +3,7 @@ const app = express();
 const PORT = 3001;
 const { readFile } = require("fs");
 
-
-
 app.use(express.json());
-
 
 app.get("/",(req,res)=> {
     readFile("./home.html", "utf8", (err, html) => {
@@ -15,7 +12,6 @@ app.get("/",(req,res)=> {
 });
 
 app.get("/about",(req,res)=> {
-    // const message = ("about the company");
     readFile("./about.html","utf8",(err,html) => {
         res.send(html);
 
@@ -24,7 +20,6 @@ app.get("/about",(req,res)=> {
 });
 
 app.get("/info",(req,res)=> {
-    // const message = ("welcome to our info page");
     readFile( "./info.html","utf8",(err,html) => {
         res.send(html);
 
@@ -37,7 +32,6 @@ app.post("/FAQ", (req,res) => {
     res.send(message);
   
 });
-
 
 app.listen(PORT, () => {
     console.log(`your server is on ${PORT}`);
