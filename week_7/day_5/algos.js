9964b01e644c2190e396d0f1fc6cabfdd1f7e017
Jessicahaugen <j.rose.haugen@gmail.com>
2021-04-27 21:07:20 -0700

    algorithm practice

---
+++ week_7/day_5/algos.js
@@ -0,0 +1,74 @@
+// // const array1 = [33, 4, 88, 1, 25];
+// // console.log(array1.map((i) => i + 5).filter((i) => i % 2).length);
+
+// iterates over everything and adds 5. then if 
+[38,9,93,6,30]
+//new array
+[38,6,30]
+// .length = 3
+
+
+
+
+// // Substrings
+// // Write a function that when given string1 as one parameter and another string2 as a second parameter, checks if string2 is a substring of string1.
+// const substring = (str1, str2) => {
+// 	return result;
+// };
+// console.log(substring("concept", "cep")); // true
+// console.log(substring("aaabbbaabbab", "aba")); // false
+// console.log(substring("the brown cow jumped over the moon", "moonthe brown cow")); // true
+// console.log(substring("baananana", "nab")); // true
+// console.log(substring("this wont work", "r u shore")); // false
+
+
+const substring = (str1, str2) => {
+newstr1= (str1 + str1)
+//  newStr2 = str2.split(" ").join("")
+   //couldnt get it to check around... ugh
+ return newstr1.includes(str2)
+
+
+};
+console.log(substring("hello","hel"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Missing Digit
+// // Write a function that when given a string equation with one digit as an 'x', return the value of 'x' that makes the equation true.
+// const findX = (equation) => {
+// 	return x;
+// };
+// // Part 1
+// console.log(findX("10 * x = 20")); // returns 2
+// console.log(findX("2 + 4 = x")); // returns 6
+// console.log(findX("x - 11 = 33")); // returns 4
+// // Part 2
+// console.log(findX("1x0 * 2 = 200")); // returns 0
+// console.log(findX("80 / 2 = x0")); // returns 4
+// console.log(findX("111 + 11x = 222")); // returns 1
+
+
+// const findX = (equation) => {
+//     let x = ;
+//     newE = eval(equation)
+//     console.log(newE)
+//     // for( let x = 0; x< 1000;x++){
+        
+// };
+// console.log((findX("10 * x = 20")))
+
