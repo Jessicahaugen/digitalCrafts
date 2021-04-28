463f42babb945dce766438f5eedcbc565ed19364
Jessicahaugen <j.rose.haugen@gmail.com>
2021-04-27 21:06:33 -0700

    CRD no U

--- week_8/day_1/server/index.js
+++ week_8/day_1/server/index.js
@@ -3,15 +3,29 @@ const app = express();
 const PORT = 3004;
 const cors = require("cors");
 const pool = require("./db.js");
-
-app.use(express.json())
-app.use(cors())
+const es6Renderer = require("express-es6-template-engine");
 
 
-app.get("/", (req,res) => {
-    
+app.use(express.json());
+app.use(cors());
+app.engine("html", es6Renderer);
+app.set("views", "../template")
+app.set("view engine", "html")
+app.use(express.urlencoded({
+  extended: true
+}))
+
+const ROOT = "/home";
+
+app.get("/home", async (req, res) => {
+  const recipes = await pool.query(
+    "SELECT * from recipes ORDER BY cookbook_id DESC"
+  );
+
+  res.render("home", { locals: { recipes: recipes.rows }});
 });
-app.post("/recipes", async (req, res) => {
+
+app.post("/recipe", async (req, res) => {
     try {
         const {recipe,vegan} = req.body;
         
@@ -19,24 +33,24 @@ app.post("/recipes", async (req, res) => {
             "INSERT INTO recipes (recipe,vegan) VALUES($1,$2)",
             [recipe,vegan]
             );
-            res.json(newRecipe.rows);
+        res.redirect(ROOT);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-app.get("/", async (req, res) => {
+app.get("/recipes", async (req, res) => {
     try {
         const getRecipes = await pool.query(
         "SELECT * from recipes ORDER BY cookbook_id"
         );
-      res.json(getRecipes.rows);
+      res.render("recipes", { locals: { recipes: getRecipes.rows } });
     } catch (err) {
         console.log(err.message);
     }
 });
 
-app.put("/update/", async (req, res) => {
+app.post("/update/:id", async (req, res) => {
     try {
       const { id } = req.params;
       const {recipe,vegan} = req.body;
@@ -45,26 +59,24 @@ app.put("/update/", async (req, res) => {
         "UPDATE recipes SET (recipe,vegan) = ($1,$2) WHERE cookbook_id = $3",
         [recipe,vegan,id]
     );
-      res.json("you have updated your recipe");
+      res.redirect(ROOT_PAGE);
     } catch (err) {
       console.error(err.message);
     }
 });
 
-app.delete("/delete/:id", async (req, res) => {
+app.post("/delete/:id", async (req, res) => {
     try {
       const { id } = req.params;
       const deleteRecipe = await pool.query(
         "DELETE FROM recipes WHERE cookbook_id = $1",
         [id]
       );
-      res.json("recipe has been deleted");
+      res.redirect(ROOT);
     } catch (err) {
       console.log(err.message);
     }
 });
-
-
 
 app.listen(PORT,() =>  {
     console.log(`listening on ${PORT}`)
